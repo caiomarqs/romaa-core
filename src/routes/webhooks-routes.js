46a46aa8e25403e56c97694118c148cc7badb1c2
caiomarqs <caiomarques1412@gmail.com>
2022-04-15 14:18:53 -0300

    Add all orders webhooks routes

--- src/routes/webhooks-routes.js
+++ src/routes/webhooks-routes.js
@@ -3,7 +3,22 @@ import express from "express"
 const router = express.Router()
 
 router.post('/order-created', async (req, res) => {
-    console.log(req)
+    console.log(req.body.toString())
+    res.send('ok')
+})
+
+router.post('/order-paid', async (req, res) => {
+    console.log(req.body.toString())
+    res.send('ok')
+})
+
+router.post('/order-updated', async (req, res) => {
+    console.log(req.body.toString())
+    res.send('ok')
+})
+
+router.post('/order-refunded', async (req, res) => {
+    console.log(req.body.toString())
     res.send('ok')
 })
 
