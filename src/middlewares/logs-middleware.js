b8223065291bc8a1c13c0646dd0d8111d1c7ee4b
caiomarqs <caiomarques1412@gmail.com>
2022-04-16 19:13:19 -0300

    Config logger to errors

--- src/middlewares/logs-middleware.js
+++ src/middlewares/logs-middleware.js
@@ -2,30 +2,38 @@ import fs from 'fs'
 import morgan from 'morgan'
 
 const date = new Date()
-const logFile = `./log${date.getMonth()}${date.getDate()}-log`
+const logFile = `./log-${date.getFullYear()}${date.getMonth()+1}${date.getDate()}.log`
 
 const applicationLogger = (app) => {
-    app.use(morgan('common', {
-        stream: fs.createWriteStream(logFile, { flags: 'a' })
-    }));
+    morgan.token('custom-errors', (req, res) => (res.statusCode > 300 ? `\"Error\"` : ''))
+    app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :custom-errors', {
+        stream: fs.createWriteStream(logFile, { flags: 'a' }),
+    }))
     app.use(morgan('tiny'))
 }
 
 const webhooksLogger = (req, res, next) => {
     const { event } = req.body
-    const objectEvent = event.split('.')[0]
+    const objectEvent = event?.split('.')[0]
 
     switch (objectEvent) {
         case "order":
-            break;
-        case "order":
-            break;
+            const { order } = req.body
+            writeLog(`${event} on ${order.order_number}`)
+        case "product":
+            () => { }
         default:
-            fs.createWriteStream(logFile, { flags: 'a' })
-            console.log()
+            writeLog("\"No indetify webhook was called!\" ", false)
     }
 
     next()
+}
+
+const writeLog = (message, consoleStream = true) => {
+    fs.appendFileSync(logFile, message)
+    if (consoleStream === true) {
+        console.log(message)
+    }
 }
 
 const LoggerMiddleware = {
