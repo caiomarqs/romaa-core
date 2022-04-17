7cf87145a825bfd6cc4bbbc310a3b4eda3b4ad2c
caiomarqs <caiomarques1412@gmail.com>
2022-04-17 11:05:23 -0300

    Import OrdersJupyter

--- src/services/jupyter-service.js
+++ src/services/jupyter-service.js
@@ -1,15 +1,18 @@
 import { OrdersCollection } from "../data"
-import { OrdersWithNoClient } from "../models/Responses"
+import {
+    OrdersJupyter,
+    OrdersWithNoClient
+} from "../models/Responses"
 
 const getAllOrders = async () => {
     const orders = await OrdersCollection.getAllOrders()
-    const models = orders.map(order =>  new OrdersJupyter(order))
+    const models = orders.map(order => new OrdersJupyter(order))
     return models
 }
 
 const getAllOrdersNoClients = async () => {
     const orders = await OrdersCollection.getAllOrders()
-    const models = orders.map(order =>  new OrdersWithNoClient(order))
+    const models = orders.map(order => new OrdersWithNoClient(order))
     return models
 }
 
